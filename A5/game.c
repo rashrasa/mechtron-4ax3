b07fa9f44e2f1bd7197d61f9fac42b3e49789705
rashrasa <ahmed.rashrash@gmail.com>
2025-11-30 18:51:38 -0500

    first running version

--- A5/game.c
+++ A5/game.c
@@ -4,18 +4,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdbool.h> 
+#include <stdbool.h>
+#include <string.h>
+#include <math.h>
 
-int  score;   /* Globals */
 char in[20];
 
-const char VALUE_LIST_PATH[] = "./current_value_states.csv";
-
 // Learning
-const bool LEARNING_MODE = true;
-const double REWARD_DISCOUNT = 0.1;
+const double TERMINATION_REWARD = 1;
+const double GAMMA = 0.1;
+const double TOLERANCE = 1e-9;
 
-int user(void)
+int user(int score)
 {
 	int i = 0;
 	while (i != 1 && i != 2) {
@@ -27,7 +27,7 @@ int user(void)
 }
 
 
-int computer(void)
+int computer(int score)
 {
 	int i;
 	if (score % 3 == 1)  i = 1;
@@ -38,7 +38,7 @@ int computer(void)
 }
 
 
-int computer2(void)
+int computer2(int score)
 {
 	if (score % 3 == 1) return(1);
 	if (score % 3 == 0)return(2);
@@ -46,60 +46,99 @@ int computer2(void)
 }
 
 // returns 1 or 2 - greedy policy
-int valueIterationComputer(float values[21], int current_state) {
+int valueIterationComputer(float values[42], int state) {
 	// Invalid state
-	if (current_state < 0 | current_state >= 20) {
-		printf("Program crashed: Invalid input state during computer's turn.");
+	bool agent_turn = state % 2 == 0;
+	int score = state / 2;
+	if (score < 0 | score >= 20 | !agent_turn) {
+		printf("Game crashed: Invalid input state during value iteration computer's turn. State: %d\n", state);
 		exit(1);
 	}
 	// 1 and 2 are valid moves
-	if (current_state + 2 <= 20) {
-		return (values[current_state + 1] > values[current_state + 2]) ? 1 : 2;
+	if (score + 2 <= 20) {
+		return (values[state + 1] > values[state + 3]) ? 1 : 2;
 	}
 	// Only 1 is a valid move
-	else if (current_state + 1 <= 20) {
+	else if (score + 1 <= 20) {
 		return 1;
 	}
 	else {
-		printf("Game crashed: Invalid state reached: State %d", current_state);
+		printf("Game crashed: Invalid state reached: State %d\n", state);
 		exit(1);
 	}
 }
 
-void runValueIteration(float values[21]) {
-	// V(s) = max_a(Rss'a + GAMMA*V(s'))
+// Mutates array with updated values
+void runValueIteration(float values[42]) {
+	float old_values[42];
+	float improvement = 0;
+
+	do {
+		// Store old values
+		memcpy(old_values, values, 42 * sizeof(float));
+
+		improvement = 0;
+		for (int i = 0; i < 40; i++) { // Iterate over NON terminal states
+			// V(s) = max_a(Rss'a + GAMMA*V(s'))
+			bool agent_turn = i % 2 == 0;
+			int score = i / 2;
+			
+			// Calculate next state
+			int s_prime;
+			if (agent_turn) {
+				s_prime = i + ((valueIterationComputer(old_values, i) == 2) ? 3 : 1);
+			}
+			else {
+				s_prime = i + ((computer2(score) == 2) ? 3 : 1);
+			}
+			// Termination states
+			float rSSA = 0;
+			if (s_prime == 40) {
+				rSSA = +TERMINATION_REWARD;
+			}
+			else if (s_prime == 41) {
+				rSSA = -TERMINATION_REWARD;
+			}
+			values[i] = rSSA + GAMMA * old_values[s_prime];
+
+			// Evaluate
+			improvement = max(abs(old_values[i] - values[i]), improvement);
+		}
+	} while (improvement > TOLERANCE);
+
 }
 
 
 int main(void)
 {
-	FILE* values_file;
-	float values[21]; // init to zeroes
-	fopen_s(values_file, VALUE_LIST_PATH, 'r');
+	// states are encoded as:
+	// i % 2 == 0 -> agent turn
+	// i // 2 -> score
 
-	if (LEARNING_MODE) {
-		// TODO
-		runValueIteration(values_file);
-		return 0;
+	float values[42]; // implicit init to zeroes
+	runValueIteration(values); // learns values on each run
+	for (int i = 0; i < 42; i++) {
+		printf("% .4f ", values[i]);
 	}
+	printf('\n');
+
 	srand(time(NULL));
 	int i;
 	printf(" Who says first 20 \n \n");
-	score = 0;
+	int score = 0;
 	i = 0;
 	while (i != 1 && i != 2) {
 		printf("Who goes first: you=1 computer=2 ? ");
 		fgets(in, 10, stdin);
 		i = atoi(in);
 	};
-	if (i == 2)score = computer();
+	if (i == 2)score = valueIterationComputer(values, score);
 	while (score <= 20) {
-		score = score + user();
+		score = score + user(score);
 		if (score >= 20) { printf(" YOU WIN !!\n "); break; };
-		score = score + computer();
+		score = score + valueIterationComputer(values, score);
 		if (score >= 20) { printf(" I WIN !! \n "); break; };
 	};
-	close(values_file);
 	return 0;
 };
 
