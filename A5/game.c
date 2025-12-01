0fbe7635361d6bf29d5f96b5a493f34f29543109
rashrasa <ahmed.rashrash@gmail.com>
2025-11-30 20:51:12 -0500

    fixed crash

--- A5/game.c
+++ A5/game.c
@@ -12,10 +12,17 @@ char in[20];
 
 // Learning
 const double TERMINATION_REWARD = 1;
-const double GAMMA = 0.1;
-const double TOLERANCE = 1e-9;
+const double GAMMA = 0.9;
+const double TOLERANCE = 1e-2;
 
-int user(int score)
+// states are encoded as:
+// i % 2 == 0 -> agent turn
+// i // 2 -> score
+// scores: 0-21 (can add 2 at 19)
+// length: 22 * 2
+#define NUM_STATES 44
+
+int user(unsigned int score)
 {
 	int i = 0;
 	while (i != 1 && i != 2) {
@@ -27,7 +34,7 @@ int user(int score)
 }
 
 
-int computer(int score)
+int computer(unsigned int score)
 {
 	int i;
 	if (score % 3 == 1)  i = 1;
@@ -38,19 +45,19 @@ int computer(int score)
 }
 
 
-int computer2(int score)
+int computer2(unsigned int score)
 {
 	if (score % 3 == 1) return(1);
-	if (score % 3 == 0)return(2);
-	else return(rand() % 2 + 1);
+	else if (score % 3 == 0)return(2);
+	else return(1);
 }
 
 // returns 1 or 2 - greedy policy
-int valueIterationComputer(float values[42], int state) {
+int valueIterationComputer(float values[NUM_STATES], unsigned int state) {
 	// Invalid state
 	bool agent_turn = state % 2 == 0;
-	int score = state / 2;
-	if (score < 0 | score >= 20 | !agent_turn) {
+	unsigned int score = state / 2;
+	if ((score < 0) || (score >= 20) || (!agent_turn)) {
 		printf("Game crashed: Invalid input state during value iteration computer's turn. State: %d\n", state);
 		exit(1);
 	}
@@ -69,19 +76,19 @@ int valueIterationComputer(float values[42], int state) {
 }
 
 // Mutates array with updated values
-void runValueIteration(float values[42]) {
-	float old_values[42];
+void runValueIteration(float values[NUM_STATES]) {
+	float old_values[NUM_STATES];
 	float improvement = 0;
 
 	do {
 		// Store old values
-		memcpy(old_values, values, 42 * sizeof(float));
+		memcpy(old_values, values, NUM_STATES * sizeof(float));
 
 		improvement = 0;
 		for (int i = 0; i < 40; i++) { // Iterate over NON terminal states
 			// V(s) = max_a(Rss'a + GAMMA*V(s'))
 			bool agent_turn = i % 2 == 0;
-			int score = i / 2;
+			unsigned int score = i / 2;
 			
 			// Calculate next state
 			int s_prime;
@@ -93,10 +100,10 @@ void runValueIteration(float values[42]) {
 			}
 			// Termination states
 			float rSSA = 0;
-			if (s_prime == 40) {
+			if (s_prime/2 >= 20 && s_prime % 2 == 1) { // You win if the score is >=20 and it's the opponent's turn (meaning you just played your turn)
 				rSSA = +TERMINATION_REWARD;
 			}
-			else if (s_prime == 41) {
+			else if (s_prime / 2 >= 20 && s_prime % 2 == 0) {
 				rSSA = -TERMINATION_REWARD;
 			}
 			values[i] = rSSA + GAMMA * old_values[s_prime];
@@ -105,25 +112,20 @@ void runValueIteration(float values[42]) {
 			improvement = max(abs(old_values[i] - values[i]), improvement);
 		}
 	} while (improvement > TOLERANCE);
-
 }
 
 
 int main(void)
 {
-	// states are encoded as:
-	// i % 2 == 0 -> agent turn
-	// i // 2 -> score
-
-	float values[42]; // implicit init to zeroes
+	float values[NUM_STATES]; // implicit init to zeroes
 	runValueIteration(values); // learns values on each run
-	for (int i = 0; i < 42; i++) {
-		printf("% .4f ", values[i]);
+	printf("Score\tAgent\tOpponent\n");
+	for (unsigned int i = 0; i < NUM_STATES/2; i+=2) {
+		printf("%d\t%.4f\t%.4f\n", i/2, values[i*2], values[i*2+1]);
 	}
-	printf('\n');
 
 	srand(time(NULL));
-	int i;
+	unsigned int i;
 	printf(" Who says first 20 \n \n");
 	int score = 0;
 	i = 0;
